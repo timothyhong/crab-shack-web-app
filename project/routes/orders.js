e3062dcc4ac9ec02aa41d57c41f1a68cf58515f2
Tim <61671973+timothyhong@users.noreply.github.com>
2021-05-07 21:04:35 -0400

    started working on Orders page, going to need to rework functions that assume 1 table per page

--- project/routes/orders.js
+++ project/routes/orders.js
@@ -3,6 +3,20 @@ const mysql = require('../dbcon.js');
 const funcs = require('../index.js');
 let router = express.Router();
 
+// main page route
+router.route("/").get((req, res) => {
+	let context = {};
+	let data = {};
+	data.cols = ["product_type_description", "product_type_code"];
+	getProducts().then(rows => {
+		context.rows = rows;
+	}).then(() => funcs.getColumns(data, "Ref_Product_Types", false)).then(rows => {
+		context.productTypes = rows;
+		res.render('products', context);
+	}).catch(err => console.error(err));
+});
+
+
 router.route("/create").get((req, res) => {
 	let context = {};
 	funcs.getData("Ref_Card_Types").then(rows => {
