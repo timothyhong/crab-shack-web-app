7697bec43dffafd72bd00cdcadf6b7e8e3e0ebb3
Tim <61671973+timothyhong@users.noreply.github.com>
2021-06-07 13:41:19 -0400

    Removed user/pass from dbcon.js

--- project/dbcon.js
+++ project/dbcon.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 const pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_hongti',
-  password        : '9183',
-  database        : 'cs340_hongti'
+  user            : 'YOUR_USERNAME_HERE',
+  password        : 'YOUR_PASSWORD_HERE',
+  database        : 'YOUR_DB_HERE'
 });
 
 module.exports.pool = pool;
